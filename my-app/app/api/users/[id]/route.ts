7030275d30ff276d251e9f88a43e5f279e9c5460
selmi-ahlem <selmiahlem1999@gmail.com>
2023-09-18 10:21:45 +0200

    Update User Informations

--- my-app/app/api/users/[id]/route.ts
+++ my-app/app/api/users/[id]/route.ts
@@ -1,62 +1,106 @@
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
-import type { NextApiRequest } from 'next';
+import type { NextApiRequest } from "next";
+import { hash } from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary";
 
-export const GET = async (req: NextApiRequest, { params }: { params: { id?: string } }) => {
-    try {
-        if (!params || !params.id) {
-            throw new Error("ID parameter is missing");
-        }
-
-        const { id } = params;
-        const user = await prisma.user.findUnique({
-            where: { id },
-        });
-
-        if (!user) {
-            return NextResponse.json({ error: "User not found" }, { status: 404 });
-        }
-
-        return NextResponse.json(user);
-    } catch (err: unknown) {
-        const message = err instanceof Error ? err.message : 'An unknown error occurred';
-        return NextResponse.json({ message: "Error fetching user", error: message }, { status: 500 });
-    }
+export const config = {
+  api: {
+    bodyParser: true,
+  },
 };
 
-export const PUT = async (req: NextApiRequest, { params }: { params: { id?: string } }) => {
-    try {
-        if (!params || !params.id) {
-            throw new Error("ID parameter is missing");
-            
-        }
-
-        const { id } = params;
-        const data = JSON.parse(req.body as string);
-        const updatedUser = await prisma.user.update({
-            where: { id },
-            data,
-        });
-        return NextResponse.json(updatedUser);
-    } catch (err: unknown) {
-        const message = err instanceof Error ? err.message : 'An unknown error occurred';
-        return NextResponse.json({ message: "Error updating user", error: message }, { status: 500 });
+cloudinary.config({
+  cloud_name: "your_cloud_name",
+  api_key: "your_api_key",
+  api_secret: "your_api_secret",
+});
+export const GET = async (
+  req: NextApiRequest,
+  { params }: { params: { id?: string } }
+) => {
+  try {
+    if (!params || !params.id) {
+      throw new Error("ID parameter is missing");
     }
+
+    const { id } = params;
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    return NextResponse.json(user);
+  } catch (err: unknown) {
+    const message =
+      err instanceof Error ? err.message : "An unknown error occurred";
+    return NextResponse.json(
+      { message: "Error fetching user", error: message },
+      { status: 500 }
+    );
+  }
 };
 
-export const DELETE = async (req: NextApiRequest, { params }: { params: { id?: string } }) => {
-    try {
-        if (!params || !params.id) {
-            throw new Error("ID parameter is missing");
-        }
-
-        const { id } = params;
-        await prisma.user.delete({
-            where: { id },
-        });
-        return NextResponse.json({ message: "User deleted successfully" });
-    } catch (err: unknown) {
-        const message = err instanceof Error ? err.message : 'An unknown error occurred';
-        return NextResponse.json({ message: "Error deleting user", error: message }, { status: 500 });
+export const PUT = async (
+  req: Request,
+  { params }: { params: { id?: string } }
+) => {
+  try {
+    if (!params || !params.id) {
+      throw new Error("ID parameter is missing");
     }
+    const body = await req.json();
+    
+    const { id } = params;
+    let hashedPassword;
+    if (body.password) {
+      hashedPassword = await hash(body.password, 12);
+    }
+
+    const updateData: any = { ...body };
+    if (hashedPassword) {
+      updateData.password = hashedPassword;
+    } else {
+      delete updateData.password;
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: updateData,
+    });
+    return NextResponse.json(updatedUser);
+  } catch (err: unknown) {
+    const message =
+      err instanceof Error ? err.message : "An unknown error occurred";
+    return NextResponse.json(
+      { message: "Error updating user", error: message },
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async (
+  req: NextApiRequest,
+  { params }: { params: { id?: string } }
+) => {
+  try {
+    if (!params || !params.id) {
+      throw new Error("ID parameter is missing");
+    }
+
+    const { id } = params;
+    await prisma.user.delete({
+      where: { id },
+    });
+    return NextResponse.json({ message: "User deleted successfully" });
+  } catch (err: unknown) {
+    const message =
+      err instanceof Error ? err.message : "An unknown error occurred";
+    return NextResponse.json(
+      { message: "Error deleting user", error: message },
+      { status: 500 }
+    );
+  }
 };
